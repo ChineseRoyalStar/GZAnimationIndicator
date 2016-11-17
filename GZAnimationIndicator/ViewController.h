b6a3e7cf1b7ffd98bcfcc16cb2ae05a31db85b6c
Leon Guo <chineseroyalstar@gmail.com>
2016-11-17 09:49:32 +0800

    Initial Commit

---
+++ GZAnimationIndicator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZAnimationIndicator
+//
+//  Created by armada on 2016/11/17.
+//  Copyright © 2016年 com.zlot.gz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
